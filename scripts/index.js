c883b1ce705764774551ede0069036c0dd41845a
Maria Victoria <mariahb7@outlook.com>
2023-08-09 18:25:33 -0500

    ajustes diseño y funcionalidades

--- scripts/index.js
+++ scripts/index.js
@@ -1,167 +1,135 @@
+let actividad = document.getElementsByClassName('actividad')[0];
+const mensajes = document.querySelector('h1');
+const iniciar = document.getElementById('iniciar');
+const comer = document.getElementById('comer');
+const dormir = document.getElementById('dormir');
+const jugar = document.getElementById('jugar');
+
+
 class Tamagotchi {
-    constructor(comer, dormir, desparchado, edad) {
-        this.comer = comer;
+    constructor(hambre, dormir, jugar, felicidad, edad) {
+        this.hambre = hambre;
         this.dormir = dormir;
-        this.desparchado = desparchado;
+        this.jugar = jugar;
+        this.felicidad = felicidad;
         this.edad = edad;
     }
-
 }
 
-let myTamagotchi = new Tamagotchi(1, 1, 1, 0);
-
-$("#iniciar").hide();
+let myTamagotchi = new Tamagotchi(100, 100, 100, 100,0);
 
 function startGame() {
+    actividad.src='inicio.gif';
     crecer();
 }
 
-function crecer() {
-    debugger;
-    if (myTamagotchi.edad < 1) {
-        hatch();
-    } else {
-        document.getElementsByClassName('actividad')[0].src='bebe.gif';
-        incrementaEdad();
-        incrementaComer();
-        incrementaDormir();
-        incrementaDesparchado();
-    }
-}
-function hatch() {
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.edad += 1;
-        $("#age").replaceWith(`<p id='age'>Edad: ${myTamagotchi.edad}</p>`);
-        crecer();
-    }, 15000)
-}
+function evolucionar() {
+    window.setTimeout(function() {
+    myTamagotchi.edad += 1;
+    myTamagotchi.felicidad = Math.max(0, myTamagotchi.felicidad - 5);
+    myTamagotchi.hambre = Math.max(0, myTamagotchi.hambre - 3);
+    myTamagotchi.dormir = Math.max(0, myTamagotchi.dormir - 5);
+    myTamagotchi.jugar = Math.max(0, myTamagotchi.jugar - 2);
 
-function incrementaComer() {
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.Comer += 1;
-        $("#hunger").replaceWith(`<p id='hunger'>Comer: ${myTamagotchi.Comer}</p>`);
-        if (myTamagotchi.Comer < 10) {
-            incrementaComer();
-        } else {
-            muerto();
-        }
-    }, 20000)
+    $("#edad").replaceWith(`<p id='edad'>Edad: ${myTamagotchi.edad}</p>`);
+    $("#felicidad").replaceWith(`<p id='felicidad'>Felicidad: ${myTamagotchi.felicidad}</p>`);
+    $("#stats_hambre").replaceWith(`<p id='stats_hambre'>Hambre: ${myTamagotchi.hambre}</p>`);
+    $("#stats_dormir").replaceWith(`<p id='stats_dormir'>Dormir: ${myTamagotchi.dormir}</p>`);
+    $("#stats_jugar").replaceWith(`<p id='stats_jugar'>Jugar: ${myTamagotchi.jugar}</p>`);
+    
+    crecer();
+ }, 3000)
 };
 
-function incrementaDormir() {
-
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.dormir += 1;
-        $("#sleepiness").replaceWith(`<p id='sleepiness'>Dormir: ${myTamagotchi.dormir}</p>`);
-        if (myTamagotchi.dormir < 10) {
-            incrementaDormir();
-        } else {
-            muerto();
-        }
-    }, 30000)
-
+function crecer() {
+    if (myTamagotchi.edad < 80 && myTamagotchi.felicidad > 10) {
+        evolucionar();
+    } else if (myTamagotchi.felicidad >= 1 && myTamagotchi.felicidad <= 10)  {
+        mensajes.style.display = 'inline';
+        actividad.src='asustado.gif';
+        muerto(myTamagotchi.felicidad);
+   } 
 };
 
 function incrementaDesparchado() {
     let timeoutID = window.setTimeout(function() {
-        myTamagotchi.desparchado += 2;
-        $("#boredom").replaceWith(`<p id='boredom'>Desparchado: ${myTamagotchi.desparchado}</p>`);
-        if (myTamagotchi.desparchado < 10) {
+        myTamagotchi.jugar += 2;
+        $("#boredom").replaceWith(`<p id='boredom'>Desparchado: ${myTamagotchi.jugar}</p>`);
+        if (myTamagotchi.jugar < 10) {
             incrementaDesparchado();
         } else {
             muerto();
         }
-    }, 10000)
+    }, 1000)
 };
 
-function incrementaEdad() {
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.age += 1;
-        $("#age").replaceWith(`<p id='age'>Edad: ${myTamagotchi.edad}</p>`);
-        if (myTamagotchi.edad < 10) {
-            incrementaEdad();
-        }
-    }, 100000)
-
+function muerto(tiempoVida ) {
+    if (tiempoVida <= 10){
+       actividad.src='Muerto.gif';
+       $("#dormir").hide();
+       $("#jugar").hide();
+       $("#comer").hide();
+       $("#iniciar").show()
+       $("#actions").after("<h1>Tamagotchi a muerto!!!</h1>")
+ }
 }
 
-function comer() {
-    document.getElementsByClassName('actividad')[0].src='comiendo.gif';
-    if (myTamagotchi.comer >= 2) {
-        myTamagotchi.comer -= 2;
-        $("#hunger").replaceWith(`<p id='hunger'>Comer: ${myTamagotchi.comer}</p>`);
-    } else {
-        myTamagotchi.comer = 0;
-        $("#hunger").replaceWith(`<p id='hunger'>Comer: ${myTamagotchi.comer}</p>`);
-    }
-}
-
-function jugar() {
-    document.getElementsByClassName('actividad')[0].src='Jugando.gif';
-    if (myTamagotchi.desparchado >= 3) {
-        myTamagotchi.desparchado -= 3;
-        $("#boredom").replaceWith(`<p id='boredom'>Desparchado: ${myTamagotchi.desparchado}</p>`);
-    } else {
-        myTamagotchi.desparchado = 0;
-        $("#boredom").replaceWith(`<p id='boredom'>Desparchado: ${myTamagotchi.desparchado}</p>`);
-    }
-}
-
-function descansar(time) {
-    document.getElementsByClassName('actividad')[0].src='Durmiendo.gif';
-    let timeoutID = window.setTimeout(function() {
-        myTamagotchi.dormir = 0;
-        $("#sleepiness").replaceWith(`<p id='sleepiness'>Dormir: ${myTamagotchi.dormir}</p>`);
-    }, time)
-}
-
-function muerto() {
-    alive = false;
-    document.getElementsByClassName('actividad')[0].src='Muerto.gif';
-    $("#sleep").hide();
-    $("#play").hide();
-    $("#feed").hide();
-    $("#iniciar").show()
-    $("#actions").after("<h2>Tamagotchi a muerto!!!</h2>")
-}
-
-function iniciar() {
+function volver() {
     let alive = true;
     $("#iniciar").hide();
-    document.getElementsByClassName('actividad')[0].src='bebe.gif';
+    actividad.src='bebe.gif';
     myTamagotchi.dormir = 0;
-    $("#sleepiness").replaceWith(`<p id='sleepiness'>Dormir: ${myTamagotchi.dormir}</p>`);
-    $("#sleep").show()
-    myTamagotchi.desparchado = 0;
-    $("#boredom").replaceWith(`<p id='boredom'>Desparchado: ${myTamagotchi.desparchado}</p>`);
-    $("#play").show()
+    $("#sleepiness").replaceWith(`<p id='dormir'>Dormir: ${myTamagotchi.dormir}</p>`);
+    $("#dormir").show()
+    myTamagotchi.jugar = 0;
+    $("#boredom").replaceWith(`<p id='jugar'>Jugar: ${myTamagotchi.jugar}</p>`);
+    $("#jugar").show()
     myTamagotchi.comer = 0;
-    $("#hunger").replaceWith(`<p id='hunger'>Comer: ${myTamagotchi.comer}</p>`);
-    $("#feed").show()
+    $("#hunger").replaceWith(`<p id='comer'>Comer: ${myTamagotchi.comer}</p>`);
+    $("#comer").show()
     myTamagotchi.edad = 0
     $("#age").replaceWith(`<p id='age'>Edad: ${myTamagotchi.edad}</p>`);
     startGame();
 }
 
-$("#sleep").click(function() {
-    descansar(2000);
+dormir.addEventListener('click', function() {
+    actividad.src='Durmiendo.gif';
+    myTamagotchi.felicidad = Math.max(0, myTamagotchi.felicidad - 5);
+    $("#stats_dormir").replaceWith(`<p id='stats_dormir'>Dormir: ${myTamagotchi.dormir}</p>`);
 });
 
-$("#play").click(function() {
-    jugar();
+jugar.addEventListener('click', function() {
+    if (myTamagotchi.jugar >= 95) {
+        actividad.src='tranquilo.jpg';
+    } else {
+        actividad.src='Jugando.gif';
+        myTamagotchi.felicidad = Math.min(100, myTamagotchi.felicidad + 5);
+        myTamagotchi.hambre = Math.min(100, myTamagotchi.hambre + 20);
+
+        $("#felicidad").replaceWith(`<p id='felicidad'>Felicidad: ${myTamagotchi.felicidad}</p>`);
+        $("#stats_hambre").replaceWith(`<p id='stats_hambre'>Hambre: ${myTamagotchi.hambre}</p>`);
+    }
 })
 
-$("#feed").click(function() {
-    comer();
-})
+comer.addEventListener('click', function() {
+    if (myTamagotchi.comer >= 90) {
+        actividad.src='tranquilo.jpg';
+    } else {
+        actividad.src='comiendo.gif';
+        myTamagotchi.felicidad = Math.min(100, myTamagotchi.felicidad + 15);
+        myTamagotchi.hambre = Math.min(100, myTamagotchi.hambre + 5);
+        $("#stats_hambre").replaceWith(`<p id='stats_hambre'>Hambre: ${myTamagotchi.hambre}</p>`);
+    }
+}); 
 
-$("#iniciar").click(function() {
-    iniciar();
-})
+iniciar.addEventListener('click', function(){
+    iniciar.style.display = 'none';
+    comer.style.display = 'inline';
+    dormir.style.display = 'inline';
+    jugar.style.display = 'inline';
+    startGame();  
+});
 
-if (myTamagotchi.sleep > 10) {
-    muerto()
-}
 
-startGame();
+
 
